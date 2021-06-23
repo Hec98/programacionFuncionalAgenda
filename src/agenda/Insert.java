e34f8e7374c377c2a5e14731815ad4e00160b701
Hec98 <hector982015@gmail.com>
2021-06-23 15:15:46 -0500

    Add class Insert

---
+++ src/agenda/Insert.java
@@ -0,0 +1,23 @@
+package agenda;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+public class Insert {
+    public void insertar(String nombre, int telefono, String rfc, int edad) {
+        try {
+            Connection con = Conexion.getConexion();
+            PreparedStatement ps = con.prepareStatement("INSERT INTO agenda (nombre, telefono, rfc, edad) VALUES (?,?,?,?)");
+            ps.setString(1, nombre);
+            ps.setInt(2, telefono);
+            ps.setString(3, rfc);
+            ps.setInt(4, edad);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println("Error -> "+ e.toString());
+        }
+    
+    }
+}
