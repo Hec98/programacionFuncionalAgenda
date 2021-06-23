495fcb077b71c7870e641b705bdaea550c3c7e8d
Hec98 <hector982015@gmail.com>
2021-06-23 14:34:51 -0500

    Add class Conexion

---
+++ src/agenda/Conexion.java
@@ -0,0 +1,19 @@
+package agenda;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    public static Connection getConexion() {
+        String url = "jdbc:mysql://localhost:3306/agenda";
+        try {
+            Connection con = DriverManager.getConnection(url,"root","");
+            return con; 
+        } catch (SQLException e) {
+            System.err.println("Error -> "+ e.toString());
+            return null;
+        }
+               
+    }     
+}
